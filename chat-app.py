4af8125287d160a67cb557399c3e473414b9ae38
Alpha Tshibangu <alphatshibangu01@gmail.com>
2023-09-26 00:34:10 +1000

    spinner added

--- chat-app.py
+++ chat-app.py
@@ -35,87 +35,89 @@ with st.sidebar:
         st.write(load_version_history())
 
     st.info("Please refresh the browser if you decide to upload more files to reset the session", icon="🚨")
-    
-# Check if files are uploaded
-if uploaded_files:
-    # Print the number of files to console
-    print(f"Number of files uploaded: {len(uploaded_files)}")
 
-    # Load the data and perform preprocessing only if it hasn't been loaded before
-    if "processed_data" not in st.session_state:
-        # Load the data from uploaded PDF files
-        documents = []
-        for uploaded_file in uploaded_files:
-            # Get the full file path of the uploaded file
-            file_path = os.path.join(os.getcwd(), uploaded_file.name)
+with st.spinner('Wait for it...'):
+    # Check if files are uploaded
+    if uploaded_files:
+        # Print the number of files to console
+        st.write(f"Number of files uploaded: {len(uploaded_files)}")
+        print(f"Number of files uploaded: {len(uploaded_files)}")
 
-            # Save the uploaded file to disk
-            with open(file_path, "wb") as f:
-                f.write(uploaded_file.getvalue())
+        # Load the data and perform preprocessing only if it hasn't been loaded before
+        if "processed_data" not in st.session_state:
+            # Load the data from uploaded PDF files
+            documents = []
+            for uploaded_file in uploaded_files:
+                # Get the full file path of the uploaded file
+                file_path = os.path.join(os.getcwd(), uploaded_file.name)
 
-            # Use UnstructuredFileLoader to load the PDF file
-            loader = PyPDFLoader(file_path)
-            loaded_documents = loader.load_and_split()
-            print(f"Number of files loaded: {len(loaded_documents)}")
+                # Save the uploaded file to disk
+                with open(file_path, "wb") as f:
+                    f.write(uploaded_file.getvalue())
 
-            # Extend the main documents list with the loaded documents
-            documents.extend(loaded_documents)
+                # Use UnstructuredFileLoader to load the PDF file
+                loader = PyPDFLoader(file_path)
+                loaded_documents = loader.load_and_split()
+                print(f"Number of files loaded: {len(loaded_documents)}")
 
-        embeddings = OpenAIEmbeddings()
-        vectorstore = Chroma.from_documents(loaded_documents, embeddings)
+                # Extend the main documents list with the loaded documents
+                documents.extend(loaded_documents)
 
-        # Store the processed data in session state for reuse
-        st.session_state.processed_data = {
-            "document_chunks": loaded_documents,
-            "vectorstore": vectorstore,
-        }
+            embeddings = OpenAIEmbeddings()
+            vectorstore = Chroma.from_documents(loaded_documents, embeddings)
 
-        # Print the number of total chunks to console
-        print(f"Number of total chunks: {len(loaded_documents)}")
+            # Store the processed data in session state for reuse
+            st.session_state.processed_data = {
+                "document_chunks": loaded_documents,
+                "vectorstore": vectorstore,
+            }
+
+            # Print the number of total chunks to console
+            print(f"Number of total chunks: {len(loaded_documents)}")
+
+        else:
+            # If the processed data is already available, retrieve it from session state
+            document_chunks = st.session_state.processed_data["document_chunks"]
+            vectorstore = st.session_state.processed_data["vectorstore"]
+
+        # Initialize Langchain's QA Chain with the vectorstore
+        qa = ConversationalRetrievalChain.from_llm(llm,vectorstore.as_retriever())
+
+        # Initialize chat history
+        if "messages" not in st.session_state:
+            st.session_state.messages = []
+
+        # Display chat messages from history on app rerun
+        for message in st.session_state.messages:
+            with st.chat_message(message["role"]):
+                st.markdown(message["content"])
+
+        # Accept user input
+        if prompt := st.chat_input("Ask your questions?"):
+            st.session_state.messages.append({"role": "user", "content": prompt})
+            with st.chat_message("user"):
+                st.markdown(prompt)
+
+            # Query the assistant using the latest chat history
+            result = qa({"question": prompt, "chat_history": [(message["role"], message["content"]) for message in st.session_state.messages]})
+
+            # Display assistant response in chat message container
+            with st.chat_message("assistant"):
+                message_placeholder = st.empty()
+                full_response = ""
+                full_response = result["answer"]
+                message_placeholder.markdown(full_response + "|")
+                
+            # With a streamlit expander  
+            with st.expander('Document Similarity Search'):
+                # Find the relevant pages
+                search = vectorstore.similarity_search_with_score(prompt) 
+                # Write out the first 
+                st.write(search[0][0].page_content) 
+            message_placeholder.markdown(full_response) 
+            
+            print(full_response)
+            st.session_state.messages.append({"role": "assistant", "content": full_response})
 
     else:
-        # If the processed data is already available, retrieve it from session state
-        document_chunks = st.session_state.processed_data["document_chunks"]
-        vectorstore = st.session_state.processed_data["vectorstore"]
-
-    # Initialize Langchain's QA Chain with the vectorstore
-    qa = ConversationalRetrievalChain.from_llm(llm,vectorstore.as_retriever())
-
-    # Initialize chat history
-    if "messages" not in st.session_state:
-        st.session_state.messages = []
-
-    # Display chat messages from history on app rerun
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
-
-    # Accept user input
-    if prompt := st.chat_input("Ask your questions?"):
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        with st.chat_message("user"):
-            st.markdown(prompt)
-
-        # Query the assistant using the latest chat history
-        result = qa({"question": prompt, "chat_history": [(message["role"], message["content"]) for message in st.session_state.messages]})
-
-        # Display assistant response in chat message container
-        with st.chat_message("assistant"):
-            message_placeholder = st.empty()
-            full_response = ""
-            full_response = result["answer"]
-            message_placeholder.markdown(full_response + "|")
-            
-        # With a streamlit expander  
-        with st.expander('Document Similarity Search'):
-            # Find the relevant pages
-            search = vectorstore.similarity_search_with_score(prompt) 
-            # Write out the first 
-            st.write(search[0][0].page_content) 
-        message_placeholder.markdown(full_response) 
-          
-        print(full_response)
-        st.session_state.messages.append({"role": "assistant", "content": full_response})
-
-else:
-    st.write("Please upload your files.")
+        st.write("Please upload your files.")
